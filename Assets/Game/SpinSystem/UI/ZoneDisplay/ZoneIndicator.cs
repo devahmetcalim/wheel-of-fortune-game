8b922dd8dcfcd7c7ec116bd66c4db1345054e590
Ahmet Çalım <developer.ahmetcalim@gmail.com>
2025-05-11 22:37:16 +0300

    Next Super/Safe Zone Indicator
    - Next super/safe zone indicator added, updating dynamically

---
+++ Assets/Game/SpinSystem/UI/ZoneDisplay/ZoneIndicator.cs
@@ -0,0 +1,52 @@
+
+using System;
+using Game.Systems.Event;
+using TMPro;
+using UnityEngine;
+
+namespace Game.SpinSystem.UI
+{
+    public class ZoneIndicator : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI superZoneIndicatorTxt;
+        [SerializeField] private TextMeshProUGUI safeZoneIndicatorTxt;
+        private int _superZoneBaseInterval = 30;
+        private int _safeZoneBaseInterval = 5;
+        private int _currentSuperZone = 30;
+        private int _currentSafeZone = 5;
+        private void Awake()
+        {
+            _currentSuperZone = _superZoneBaseInterval;
+            _currentSafeZone = _safeZoneBaseInterval;
+            superZoneIndicatorTxt.text = _currentSuperZone.ToString();
+            safeZoneIndicatorTxt.text = _currentSafeZone.ToString();
+        }
+
+        private void OnEnable()
+        {
+            EventManager.Subscribe<SpinZoneChangedEvent>(OnSpinZoneChanged);
+        }
+
+        private void OnSpinZoneChanged(SpinZoneChangedEvent obj)
+        {
+            if (obj.SpinZone + 1 % _currentSuperZone == 0)
+            {
+                _currentSuperZone += _superZoneBaseInterval;
+                superZoneIndicatorTxt.text = _currentSuperZone.ToString();
+            }
+
+            if (obj.SpinZone + 1 % _currentSafeZone == 0)
+            {
+                _currentSafeZone += _safeZoneBaseInterval;
+                safeZoneIndicatorTxt.text = _currentSafeZone.ToString();
+            }
+        }
+
+        private void OnDisable()
+        {
+            
+            EventManager.Unsubscribe<SpinZoneChangedEvent>(OnSpinZoneChanged);
+        }
+    }
+
+}
